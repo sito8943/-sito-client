f5b268a0b37d78c087636a423b66d5649f53cee6
Sito8943 <sito8943@gmail.com>
2024-08-31 12:51:33 -0400

    created docs and syncFunction

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import { io } from "socket.io-client";
 
+/**
+ * @description Socket Client
+ */
 export class SocketClient {
-  constructor(serverUrl, options) {
-    socket = io(serverUrl, {
+  /**
+   *
+   * @param {string} serverUrl - socket url
+   * @param {function} syncFunction - function to trigger after notify event
+   * @param {object} options - socket.io-client options
+   */
+  constructor(serverUrl, syncFunction, options) {
+    this.syncFunction = syncFunction;
+    this.socket = io(serverUrl, {
       reconnectionDelayMax: options?.reconnectionDelayMax ?? 10000,
       auth: {
         token: options?.token ?? "",
       },
     });
+
+    // * Event to notify a client
+    this.socket.on("notify", syncFunction);
   }
 }
