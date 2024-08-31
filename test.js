0bc2a5877f3038bed9245d8a3b7fba988850f6c4
Sito8943 <sito8943@gmail.com>
2024-08-31 13:15:21 -0400

    fixed test for callbacks

--- test.js
+++ test.js
@@ -2,4 +2,8 @@ import { SocketClient } from "./index.js";
 
 const serverUrl = process.env.SERVER_URL;
 
-const socket = new SocketClient(serverUrl);
+const socket = new SocketClient(serverUrl, {
+  onConnected: (socket) => console.info(socket),
+  onDisconnect: (socket) => console.info(socket),
+  onNotify: (socket) => console.info(socket),
+});
